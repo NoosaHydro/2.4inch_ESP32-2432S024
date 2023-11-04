60a65b1ca9166fa3fdd586a18fb35c3027f3273c
Leo Drake <leo@leodrake.com>
2023-11-04 22:41:44 +1000

    add CST820 touchscreen demo showing gesturetype and x and y coords

--- cnd-micropython/bin/touchtest.py
+++ cnd-micropython/bin/touchtest.py
@@ -9,19 +9,18 @@ i2c.writeto_mem(21, 0xfe, b'\xff') # tell it not to sleep
 
 
 def __main__(args):
-	#num = 10
-	#pin=2
-	#if len(args) > 2:
-	#	pin = int(args[2])
+        #num = 10
+        #pin=2
+        #if len(args) > 2:
+        #       pin = int(args[2])
 
-	#print("blink pin{} ondelay{} offdelay{} loop{}".format(pin, ton, toff, num))
+        #print("blink pin{} ondelay{} offdelay{} loop{}".format(pin, ton, toff, num))
 
     while True:
-        index, gesture, x, y = struct.unpack('BBHH',i2c.readfrom_mem(21, 0x01, 6))
-        
+        index, gesture, x, y = struct.unpack('>BBHH',i2c.readfrom_mem(21, 0x01, 6)) # see https://docs.python.org/3/library/struct.html
+        xb = x & 0xFFF
+        yb = y & 0xFFF
         #data = i2c.readfrom_mem(15, 0x01, 6)
         #print(data)
-        print(index, gesture, x, y)
+        print(index, gesture, x, y,xb,yb)
         time.sleep(0.1)
-        
-        
